14add134dfed752558bc1b7ee8f4ce9e9a02ac5f
Abraham Yusuf <aaondowasey@gmail.com>
2014-11-28 15:05:21 +0100

    added bootstrap navbar

--- src/Views/Layouts/NavBar.hs
+++ src/Views/Layouts/NavBar.hs
@@ -14,14 +14,14 @@ navBar =
     div ! class_ "container-fluid" $ do
       --  Brand and toggle get grouped for better mobile display
       div ! class_ "navbar-header" $ do
-        button ! type_ "button" ! class_ "navbar-toggle collapsed" ! dataAttribute "toggle" "collapse" ! dataAttribute "target" "#bs-example-navbar-collapse-1" $ do
+        button ! type_ "button" ! class_ "navbar-toggle collapsed" ! dataAttribute "toggle" "collapse" ! dataAttribute "target" "#navbar-collapse-1" $ do
           span ! class_ "sr-only" $ "Toggle navigation"
           span ! class_ "icon-bar" $ mempty
           span ! class_ "icon-bar" $ mempty
           span ! class_ "icon-bar" $ mempty
         a ! class_ "navbar-brand" ! href "#" $ "Scotty-Angular"
           --  Collect the nav links, forms, and other content for toggling
-      div ! class_ "collapse navbar-collapse" ! id "bs-example-navbar-collapse-1" $ do
+      div ! class_ "collapse navbar-collapse" ! id "navbar-collapse-1" $ do
         ul ! class_ "nav navbar-nav" $ do
           li ! class_ "active" $ a ! href "#" $ do
             _ <- "Home"
