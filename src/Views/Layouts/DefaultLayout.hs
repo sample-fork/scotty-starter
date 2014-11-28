933a4d647a9b96be40723494520c3f09f097a588
Abraham Yusuf <aaondowasey@gmail.com>
2014-11-28 14:56:51 +0100

    added bootstrap navbar

--- src/Views/Layouts/DefaultLayout.hs
+++ src/Views/Layouts/DefaultLayout.hs
@@ -2,11 +2,13 @@ module Views.Layouts.DefaultLayout (
   defaultLayout
 ) where
 
-import Prelude hiding (div, head, id)
+import Prelude hiding (div, head, id, span)
 
 import Data.Monoid (mempty)
 import Text.Blaze.Html5 hiding (map, style)
 import Text.Blaze.Html5.Attributes hiding (title)
+
+import Views.Layouts.NavBar
 
 defaultLayout :: Html -> Html
 defaultLayout part =
@@ -19,10 +21,11 @@ defaultLayout part =
          --  Bootstrap
          link ! href "/lib/bootstrap-css/css/bootstrap.min.css" ! rel "stylesheet"
          link ! href "/lib/bootstrap-css/css/bootstrap-theme.min.css" ! rel "stylesheet"
+         link ! href "/css/style.css" ! rel "stylesheet"
     body $
-      do h1 "Hello there!"
-         part
-         div ! customAttribute "ng-view" "" $ mempty
+      do div ! class_ "wrap" $
+           do navBar
+              div ! class_ "container" ! customAttribute "ng-view" "" $ part
          --  External Javascript Libs
          script ! src "/lib/jquery/dist/jquery.min.js" $ mempty
          script ! src "/lib/jquery-ui/jquery-ui.min.js" $ mempty
---
+++ src/Views/Layouts/NavBar.hs
@@ -0,0 +1,30 @@
+module Views.Layouts.NavBar (
+  navBar
+) where
+
+import Prelude hiding (div, head, id, span)
+
+import Data.Monoid (mempty)
+import Text.Blaze.Html5 hiding (map, style)
+import Text.Blaze.Html5.Attributes hiding (title, span)
+
+navBar :: Html
+navBar =
+  nav ! class_ "navbar navbar-default" ! customAttribute "role" "navigation" $ do
+    div ! class_ "container-fluid" $ do
+      --  Brand and toggle get grouped for better mobile display
+      div ! class_ "navbar-header" $ do
+        button ! type_ "button" ! class_ "navbar-toggle collapsed" ! dataAttribute "toggle" "collapse" ! dataAttribute "target" "#bs-example-navbar-collapse-1" $ do
+          span ! class_ "sr-only" $ "Toggle navigation"
+          span ! class_ "icon-bar" $ mempty
+          span ! class_ "icon-bar" $ mempty
+          span ! class_ "icon-bar" $ mempty
+        a ! class_ "navbar-brand" ! href "#" $ "Scotty-Angular"
+          --  Collect the nav links, forms, and other content for toggling
+      div ! class_ "collapse navbar-collapse" ! id "bs-example-navbar-collapse-1" $ do
+        ul ! class_ "nav navbar-nav" $ do
+          li ! class_ "active" $ a ! href "#" $ do
+            _ <- "Home"
+            span ! class_ "sr-only" $ "(current)"
+          li $ a ! href "#" $ "Page 2"
+        ul ! class_ "nav navbar-nav navbar-right" $ li $ a ! href "#" $ "Sign In"
